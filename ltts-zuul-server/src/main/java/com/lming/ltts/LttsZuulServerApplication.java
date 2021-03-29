1a1974955a2f1293eb0c5f01924b729218fae4df
张良明 <zhangliangming@xjj.com>
2021-03-29 17:43:09 +0800

    1、gateway 初始化

---
+++ ltts-zuul-server/src/main/java/com/lming/ltts/LttsZuulServerApplication.java
@@ -0,0 +1,13 @@
+package com.lming.ltts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LttsZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LttsZuulServerApplication.class, args);
+	}
+
+}
